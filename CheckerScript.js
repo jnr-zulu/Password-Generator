9f28bdb40277a27a6537c9f3a4106d4b6793dbba
Patrick-Jnr Zulu <132206642+jnr-zulu@users.noreply.github.com>
2024-07-07 13:29:31 +0200

    Add files via upload

---
+++ CheckerScript.js
@@ -0,0 +1,28 @@
+// script.js 
+
+document.addEventListener('DOMContentLoaded', function() {
+    let password = document.getElementById("password");
+    let power = document.getElementById("power-point");
+    
+    if (password && power) {
+        password.oninput = function () {
+            let point = 0;
+            let value = password.value;
+            let widthPower = ["1%", "25%", "50%", "75%", "100%"];
+            let colorPower = ["#D73F40", "#DC6551", "#F2B84F", "#BDE952", "#3ba62f"];
+
+            if (value.length >= 6) {
+                let arrayTest = [/[0-9]/, /[a-z]/, /[A-Z]/, /[^0-9a-zA-Z]/];
+                arrayTest.forEach((item) => {
+                    if (item.test(value)) {
+                        point += 1;
+                    }
+                });
+            }
+            power.style.width = widthPower[point];
+            power.style.backgroundColor = colorPower[point];
+        };
+    } else {
+        console.error("Password or power-point element not found");
+    }
+});
